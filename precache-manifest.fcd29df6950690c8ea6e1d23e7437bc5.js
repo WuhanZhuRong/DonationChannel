47e92498ba55114de98e9cbab15a356597e0a580
xlc <xlc@users.noreply.github.com>
2020-01-30 10:11:56 +0000

    Automated deployment: Thu Jan 30 10:11:56 UTC 2020 32e8fcf089f432f97f75cb8fed5506b3f051efde

---
+++ precache-manifest.fcd29df6950690c8ea6e1d23e7437bc5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f427ee8c73bb8df19d2bea5f6fe612b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "298356cebcd557c33ae1",
+    "url": "/static/css/2.50ceb707.chunk.css"
+  },
+  {
+    "revision": "01059ac75b77fe1fb979",
+    "url": "/static/css/main.50ddd127.chunk.css"
+  },
+  {
+    "revision": "298356cebcd557c33ae1",
+    "url": "/static/js/2.8272545e.chunk.js"
+  },
+  {
+    "revision": "b1953906fcf37f5c263e0254ff985bef",
+    "url": "/static/js/2.8272545e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01059ac75b77fe1fb979",
+    "url": "/static/js/main.f4272ab8.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "a7b4c2b8b742c9c467d1ee184bfb875b",
+    "url": "/static/media/phone.a7b4c2b8.png"
+  }
+]);
