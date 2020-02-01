fba2c00ff161ef0e04601276086ede554eb8ed26
wfnuser <wfnuser@126.com>
2020-02-01 17:00:57 +0800

    fix: styles

--- src/routes/detail/index.js
+++ src/routes/detail/index.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { WhiteSpace, Card, Icon, List, Modal, NavBar, Toast } from "antd-mobile";
+import {
+  Card,
+  Icon,
+  List,
+  Modal,
+  NavBar,
+  Toast
+} from "antd-mobile";
 import "./style.css";
 import { API_GET_HOSPITAL_BY_ID, get } from "../../utils/api";
-import copy_img from "../../assets/copy.png";
-import phone_img from "../../assets/phone.png";
 import copy from "copy-to-clipboard";
 
 class Detail extends React.Component {
@@ -20,8 +25,10 @@ class Detail extends React.Component {
   }
 
   fetchHospitalById(id) {
-    console.log('===fetchHospitalById,发起请求===', id);
-    get(API_GET_HOSPITAL_BY_ID(id)).then(res => this.setState({hospital: res.data.data[0]}));
+    console.log("===fetchHospitalById,发起请求===", id);
+    get(API_GET_HOSPITAL_BY_ID(id)).then(res =>
+      this.setState({ hospital: res.data.data[0] })
+    );
   }
 
   copyToClickBoard = (res, type) => {
@@ -34,10 +41,12 @@ class Detail extends React.Component {
 
   render() {
     const { hospital } = this.state;
-    if(!hospital || !Object.keys(hospital).length) {
-      return ''
+    if (!hospital || !Object.keys(hospital).length) {
+      return "";
     }
-    const supplyWithCategories = divideSuppliesIntoCategories(hospital.supplies);
+    const supplyWithCategories = divideSuppliesIntoCategories(
+      hospital.supplies
+    );
 
     return (
       <div>
@@ -58,25 +67,24 @@ class Detail extends React.Component {
                   <h3 className="detail-card-header-left-name">
                     {hospital.name}
                   </h3>
-                  <WhiteSpace />
-                  <span className="detail-card-header-left-address">
-                    地址： {hospital.address}
-                  </span>
+
+                  <div className="detail-card-header-left-address">
+                    <p>地址： {hospital.address}</p>
+                  </div>
                 </div>
                 <div className="detail-card-header-right">
                   <span
                     className="detail-card-header-right-phone"
                     onClick={() => this.setState({ showContactBox: true })}
                   >
-                    <img src={phone_img} alt="电话" />
+                    <i className="ai-phone" />
                   </span>
                   <span className="detail-card-header-right-copy">
-                    <img
-                      src={copy_img}
+                    <i
+                      className="ai-home"
                       onClick={() =>
                         this.copyToClickBoard(hospital.address, "医院地址")
                       }
-                      alt="复制"
                     />
                   </span>
                 </div>
@@ -84,26 +92,30 @@ class Detail extends React.Component {
             }
           />
           <Card.Body className="detail-card-body">
-            {supplyWithCategories.filter(category => category.supplies.length).map(category => (
+            {supplyWithCategories
+              .filter(category => category.supplies.length)
+              .map(category => (
                 <div key={category.name} className="detail-card-body-category">
-                  <h4 className="detail-card-body-category-name">{category.name}</h4>
+                  <h4 className="detail-card-body-category-name">
+                    {category.name}
+                  </h4>
                   <List className="detail-card-body-category-list">
                     {category.supplies.map(supply => (
-                        <List.Item
-                            key={supply.name}
-                            className="detail-card-body-category-list-item"
-                            extra={
-                              <span className="detail-card-body-category-list-item-count">
-                          {supply.count || '不限'}
-                        </span>
-                            }
-                        >
-                          {supply.name}
-                        </List.Item>
+                      <List.Item
+                        key={supply.name}
+                        className="detail-card-body-category-list-item"
+                        extra={
+                          <span className="detail-card-body-category-list-item-count">
+                            {supply.count || "不限"}
+                          </span>
+                        }
+                      >
+                        {supply.name}
+                      </List.Item>
                     ))}
                   </List>
                 </div>
-            ))}
+              ))}
           </Card.Body>
         </Card>
         <Modal
@@ -133,22 +145,30 @@ function divideSuppliesIntoCategories(supplies) {
   const armors = [];
   const equipments = [];
   const others = [];
-  supplies.split('、').forEach(str => {
-    const supply = {name: str};
-    if(str.includes('口罩')){
+  supplies.split("、").forEach(str => {
+    const supply = { name: str };
+    if (str.includes("口罩")) {
       masks.push(supply);
-    }else if(str.includes('眼') || str.includes('帽') || str.includes('面') || str.includes('衣') || str.includes('服')  ) {
+    } else if (
+      str.includes("眼") ||
+      str.includes("帽") ||
+      str.includes("面") ||
+      str.includes("衣") ||
+      str.includes("服")
+    ) {
       armors.push(supply);
-    }else if(str.includes('设备')){
-      equipments.push(supply)
-    }else {
+    } else if (str.includes("设备")) {
+      equipments.push(supply);
+    } else {
       others.push(supply);
     }
   });
-  return [{name: '口罩', supplies: masks},
-    {name: '防护', supplies: armors},
-    {name: '医用设备', supplies: equipments},
-    {name: '其他', supplies: others}];
+  return [
+    { name: "口罩", supplies: masks },
+    { name: "防护", supplies: armors },
+    { name: "医用设备", supplies: equipments },
+    { name: "其他", supplies: others }
+  ];
 }
 
 export default Detail;
